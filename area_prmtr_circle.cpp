00bf8817db8d7aa8e6d241f640706315dd8f9ae5
SaniaSarfraz76 <saniasarfraz714@gmail.com>
2025-08-01 16:24:28 +0500

    Code for area and perimeter of circle

---
+++ area_prmtr_circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    const double pi=3.14159;
+double r;
+cout<<"Input r ="<<endl;
+cin>>r;
+
+cout<<"Perimeter of circle ="<<2*pi*r<<endl;
+cout<<"Area of circle ="<<pi*r*r;
+    return 0;
+}
