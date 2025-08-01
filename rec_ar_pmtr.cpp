8d7bb5664e706fbf0a01792f9cd1807fb9c7bf04
SaniaSarfraz76 <saniasarfraz714@gmail.com>
2025-08-01 16:17:11 +0500

    Area and perimeter of rectangle concept

---
+++ rec_ar_pmtr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,b;
+cout<<"Input a"<<endl;
+cin>>a;
+cout<<"Input b"<<endl;
+cin>>b;
+cout<<"Perimeter of rectangle ="<<2*a+2*b<<endl;
+cout<<"Area of rectangle ="<<a*b;
+    return 0;
+}
