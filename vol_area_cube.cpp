224f5fc006517dbb3381a7c69334623f0e511cfd
SaniaSarfraz76 <saniasarfraz714@gmail.com>
2025-08-01 16:38:50 +0500

    Code for volume and area of cube concept

---
+++ vol_area_cube.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double l;
+    cout << "Input l = " << endl;
+    cin >> l;
+
+    cout << "Area of CUBE = " << 6 * l * l << endl;
+    cout << "Volume of CUBE = " << l * l * l << endl;
+
+    return 0;
+}
