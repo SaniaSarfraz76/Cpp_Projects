fa70159d1259a6812f7eeab856f761fe111bf558
SaniaSarfraz76 <saniasarfraz714@gmail.com>
2025-08-01 16:14:28 +0500

    Code for perimeter of square and area of square

---
+++ perimeter_area.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+  using namespace std;
+
+  int main() {
+ int s;
+  cout<<"Input side S =";
+  cin>>s;
+ cout<<"Perimeter of square ="<<4*s<<endl;
+  cout<<"Area of square ="<<s*s;
+    return 0;
+}
