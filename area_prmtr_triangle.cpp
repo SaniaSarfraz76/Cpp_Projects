8019fb3b7b8d301ec7b9e3803b96f2d63c6c318d
SaniaSarfraz76 <saniasarfraz714@gmail.com>
2025-08-01 16:28:33 +0500

    Code for area and perimeter of triangle concept

---
+++ area_prmtr_triangle.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+     int a,b,c;
+cout<<"Input a,b,c ="<<endl;
+cin>>a;
+cin>>b;
+cin>>c;
+
+     int base,height;
+cout<<"Input base,height="<<endl;
+cin>>base;
+cin>>height;
+
+cout<<"Perimeter of triangle ="<<a+b+c<<endl;
+
+cout<<"Area of triangle ="<<(0.5)*base*height;
+
+    return 0;
+}
