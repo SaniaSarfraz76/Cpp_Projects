de5ed99e060e6eded368d39a2ab03edb376d9cca
SaniaSarfraz76 <saniasarfraz714@gmail.com>
2025-08-01 16:34:11 +0500

    Code for area and perimeter of rectangular box

---
+++ area_prmtr_recbox.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+ cout<<"Input a,b,c"<<endl;
+ cin>>a>>b>>c;
+ cout<<"PERIMETER OF RECTANGULAR BOX ="<<2*a*b+2*a*c+2*b*c<<endl;
+ cout<<"VOLUME OF RECTANGULAR BOX="<<a*b*c<<endl;
+    return 0;
+}
