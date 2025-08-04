0136949427023084335f95a1d4c1b6d174e5fd68
SaniaSarfraz76 <saniasarfraz714@gmail.com>
2025-08-04 16:39:29 +0500

    Code to display table of number entered by user using while loop concept

---
+++ table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,c;
+    cout<<"Enter value of n : "<<endl;
+    cin>>n;
+    int c=1;
+    while(c<=1000){
+        cout<<n<<"*"<<c<<"n*c"<<endl;
+    }
+    return 0;
+}
