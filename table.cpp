731e33040335d4dd256bc2a2d49f99385d8ea88a
SaniaSarfraz76 <saniasarfraz714@gmail.com>
2025-08-05 16:23:39 +0500

    Code using while loop  concept to display odd numbers from 1 to 99

--- table.cpp
+++ table.cpp
@@ -1,13 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n,c;
     cout<<"Enter value of n : "<<endl;
     cin>>n;
-    int c=1;
+    c=1;
     while(c<=1000){
-        cout<<n<<"*"<<c<<"n*c"<<endl;
+        cout<<n<<"*"<<c<<"="<<n*c<<endl;
+        c++;
     }
     return 0;
 }
